44d403a555278fe3b337bf9de85dc965e88357a7
xmNuc <xmnuc@o2.pl>
2021-12-14 00:56:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2/promise');
+
+(async () => {
+  const pool = await mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'megak_courses',
+    decimalNumbers: 'true',
+    namedPlaceholders: 'true',
+  });
+
+  const [results] = await pool.execute('SELECT * FROM `courses`');
+  console.log(results);
+
+  const [results2] = await pool.execute(
+    'SELECT `students`.`id`, `students`.`firstName`, `students`.`lastName`,`students`.`courseName` FROM `students` WHERE `age` > 18'
+  );
+  console.log(results2);
+})();
